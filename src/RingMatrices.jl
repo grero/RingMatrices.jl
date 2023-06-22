358ece0e843f67d82e874acb14967e37887f42e3
Roger Herikstad <roger.herikstad@gmail.com>
2023-06-22 13:53:53 +0800

    Initial commit

---
+++ src/RingMatrices.jl
@@ -0,0 +1,102 @@
+module RingMatrices
+
+"""
+A Matrix with 1.0 along the -1 diagonal
+"""
+struct RingMatrix{T<:Real} <: AbstractMatrix{T}
+    p::T
+    n::Int64
+    index::Vector{CartesianIndex}
+    entries::Vector{T}
+end
+
+function RingMatrix(p::T, n) where T <: Real
+    # compute the valid indices
+    ni = (n-1) + 2
+    indices = Vector{CartesianIndex}(undef, ni)
+    entries = Vector{Float64}(undef, ni)
+    k = 1
+    for ii in CartesianIndices((1:n,1:n))
+        if ii[1] == ii[2]==1
+            indices[k] = ii
+            entries[k] = 1-p
+            k += 1
+        elseif ii[1]==2&&ii[2]==1
+            indices[k] = ii
+            entries[k] = p 
+            k += 1
+        elseif ii[1]==ii[2]+1
+            indices[k] = ii
+            entries[k] = one(T)
+            k+=1
+        elseif ii[1]==1&&ii[2]==n
+            indices[k] = ii
+            entries[k] = one(T) 
+        end
+    end
+    RingMatrix(p,n,indices,entries)
+end
+
+Base.size(X::RingMatrix{T}) where T <: Real = (X.n,X.n)
+
+function Base.getindex(X::RingMatrix{T}, ii::CartesianIndex) where T <: Real    
+    if ii in CartesianIndices((1:X.n, 1:X.n))
+       jj = findfirst(k->k==ii, X.index)
+       if jj === nothing
+        return 0.0
+       end
+       X.entries[jj]
+    end
+end
+
+function Base.getindex(X::RingMatrix{T}, i::Integer,j::Integer) where T <: Real
+    if ((i < 1 || i > X.n) || (j < 1 || j > X.n))
+        throw(BoundsError(X, [i,j]))
+    end
+    if i==j==1
+        return 1-X.p
+    end
+    if i==1&&j==X.n
+        return one(T)
+    end
+    if i==2&&j==1
+        return X.p
+    end
+    if i==j+1
+        return one(T)
+    end
+    return zero(T) 
+end
+
+"""
+Product of two RingMatrices
+"""
+struct RingProductMatrix{T<:Real} <: AbstractMatrix{T}
+    f1::RingMatrix{T}
+    f2::RingMatrix{T}
+    n::Int64
+end
+
+RingProductMatrix(f1::RingMatrix{T}, f2::RingMatrix{T}) where T <: Real = RingProductMatrix(f1,f2, f1.n*f2.n)
+Base.size(X::RingProductMatrix{T}) where T <: Real = (X.n, X.n)
+
+function Base.getindex(X::RingProductMatrix{T}, i::Integer, j::Integer) where T <: Real
+    p1 = X.f1.p
+    n1 = X.f1.n
+    n2 = X.f2.n
+    p2 = X.f2.p
+    n = X.n
+    n1n1 = n1*n1
+    # convert from product index to individual index
+    # find the block
+    i1 = div(i-1,n1)+1
+    j1 = div(j-1,n2)+1
+
+    #find the index within the block
+    i2 = i - (i1-1)*n1
+    j2 = j - (j1-1)*n2
+
+    X.f1[i1,j1]*X.f2[i2,j2]
+end
+
+end # module RingMatrices
---
+++ test/runtests.jl
@@ -0,0 +1,44 @@
+using RingMatrices
+using Test
+
+
+@testset "Basic" begin
+    Q = RingMatrices.RingMatrix(0.9, 4)
+    @test Q.index == [CartesianIndex(1,1), CartesianIndex(2,1),
+                     CartesianIndex(3,2), CartesianIndex(4,3),
+                     CartesianIndex(1,4)]
+    @test Q.entries ≈ [0.1, 0.9,1.0,1.0,1.0]
+    @test Q[1,1] ≈ 0.1
+    @test Q[2,1] ≈ 0.9
+    @test Q[3,2] ≈ Q[4,3] ≈ 1.0
+    @test Q[1,4] ≈ 1.0
+    @test Q[1,2] ≈ Q[1,3] ≈ 0.0
+    @test Q[2,2] ≈ Q[2,3] ≈ Q[2,4] ≈ 0.0
+    @test Q[3,1] ≈ Q[3,3] ≈ Q[3,4] ≈ 0.0
+    @test Q[4,1] ≈ Q[4,2] ≈ Q[4,4] ≈ 0.0
+
+    Q2 = RingMatrices.RingMatrix(0.9, 4)
+
+    QQ = RingMatrices.RingProductMatrix(Q,Q2)
+    @test size(QQ) == (16,16)
+    @test QQ[1:4,1:4] ≈ Q[1,1].*Q2
+    @test QQ[5:8, 1:4] ≈ Q[2,1].*Q2
+    @test QQ[9:12, 1:4] ≈ Q[3,1].*Q2
+    @test QQ[13:16,1:4] ≈ Q[4,1].*Q2
+
+    @test QQ[1:4, 5:8] ≈ Q[1,2].*Q2
+    @test QQ[1:4, 9:12] ≈ Q[1,3].*Q2
+    @test QQ[1:4, 13:16] ≈ Q[1,4].*Q2
+
+    @test QQ[5:8, 5:8] ≈ Q[2,2].*Q2
+    @test QQ[5:8, 9:12] ≈ Q[2,3].*Q2
+    @test QQ[5:8, 13:16] ≈ Q[2,4].*Q2
+
+    @test QQ[9:12, 5:8] ≈ Q[3,2].*Q2
+    @test QQ[9:12, 9:12] ≈ Q[3,3].*Q2
+    @test QQ[9:12, 13:16] ≈ Q[3,4].*Q2
+
+    @test QQ[13:16, 5:8] ≈ Q[4,2].*Q2
+    @test QQ[13:16, 9:12] ≈ Q[4,3].*Q2
+    @test QQ[13:16, 13:16] ≈ Q[4,4].*Q2
+end
